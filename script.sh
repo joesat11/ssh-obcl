6d8a4702dcb3cb2895fd39b1e8b938b480c00a57
joesat11 <141178340+joesat11@users.noreply.github.com>
2023-09-11 17:04:17 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,7 +17,10 @@ none='\e[0m'
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "
-<h3><font color='#8BC34A'>⎆═════GCP SERVER═════⎆
+<h3><font color='#8BC34A'>
+♡   (\(\  
+    („• ֊ •„)  ♡
+━━∪∪━━━━━━━━━━GCP SERVER
 </font></h3>
 <h3><font color='#4FC3F7'>❦ ❦ imnhs ❦ ❦ 
 </font></h3>
@@ -25,10 +28,11 @@ echo "
 <h3><font color='#FFA726'> အဆင်ပြေကြပါစေ။
 </font></h3>
 
-<h3><font color='#8BC34A'>⎆═════༒❦ ༒❦ ༒═════⎆
+<h3><font color='#8BC34A'>    („• ֊ •„)  ♡
+━━∪∪━━━━━━━━━━
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
 useradd "obcl" --shell=/bin/false -M
-echo "404:404" | chpasswd
+echo "404:obcl" | chpasswd
 
 echo ""
 
